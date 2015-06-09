3d5813aad1594ef28c1447cf98a9ab97f1f64b6a
Jesse James <jrjames.pdx@gmail.com>
2015-06-09 10:27:06 -0700

    update with userfeed path and remove other entries

--- router.js
+++ router.js
@@ -1,10 +1,6 @@
 Bustle.Router.map(function() {
-  this.resource('headlines', {path:'/'});
-  this.resource('headline1');
-  this.resource('headline2');
-  this.resource('headline3');
-  this.resource('headline4');
-  this.resource('carousel');
+  this.resource('userfeed', {path:'/'});
+
 
 
 
