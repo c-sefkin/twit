c773dee512190c2274a27cbe226d92f8c0343a7e
Jesse James <jrjames.pdx@gmail.com>
2015-06-09 10:25:24 -0700

    create initial file structure

---
+++ router.js
@@ -0,0 +1,11 @@
+Bustle.Router.map(function() {
+  this.resource('headlines', {path:'/'});
+  this.resource('headline1');
+  this.resource('headline2');
+  this.resource('headline3');
+  this.resource('headline4');
+  this.resource('carousel');
+
+
+
+});
